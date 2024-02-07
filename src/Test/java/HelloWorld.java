058ac90dd567682772afc3bbd4aab1b869eb19ca
Maes-tr0 <nikita.opara.work@gmail.com>
2024-02-07 21:32:42 +0200

    Added new statement

--- src/Test/java/HelloWorld.java
+++ src/Test/java/HelloWorld.java
@@ -1,7 +1,10 @@
 package Test.java;
+import java.lang.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, Mae?srt0");
+        System.out.println("Hello, Mae?srt0");
+        Integer a = 2, b = 3;
+        System.out.print(a+b);
     }
 }
